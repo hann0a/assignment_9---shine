aea097d46427f8ef1a217f9e860ee06a5299d159
hann0a <haniamilnikel@gmail.com>
2025-05-11 14:27:02 +0200

    adding comments in server.R

--- server.R
+++ server.R
@@ -1,7 +1,6 @@
 library(shiny)
-library(ggplot2)
-library(tidyr)
-library(dplyr)
+library(ggplot2) #creating plot
+library(tidyr) #pivot_longer
 
 server <- function(input, output, session) {
     tpm_data <- read.csv("TPMs_table_100genes.csv")
@@ -25,15 +24,16 @@ server <- function(input, output, session) {
       gene_long <- gene_row %>%
         pivot_longer(cols = -GeneID, names_to = "Sample", values_to = "TPM")
       
-      #barplot - bar for each sample; y axis - TPM  
+      #barplot - bar for each sample; y axis - TPM
+      #fill = Sample because I want different color for each sample
       ggplot(gene_long, aes(x = Sample, y = TPM, fill = Sample)) +
-        geom_bar(stat = "identity")+
-        scale_fill_brewer(palette = "Blues")+
+        geom_bar(stat = "identity")+  #we want values from TPM col not calculated by ggplot
+        scale_fill_brewer(palette = "Blues")+  #color palette for my plot
         labs(
           title = "Expression of gene in TPMs",
           x = "Sample",
           y = "TPM"
         )+
-        theme_minimal()
+        theme_minimal() #plot theme - no grey background 
     })
 }
